bad3137251f28e4cd7f89e123712ee1febd6b498
LeonardoMazzoni <leonardo.mazzoni@s-mart.biz>
2026-02-02 18:58:48 +0100

    using actions file to deploy instead of dist folder

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/garick-harlamov-fanpage/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
